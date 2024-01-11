a8bf9b6a6c9d1ce6c93dc0fd27919dc273c5859c
calvin4370 <calvin.j.j.chan@gmail.com>
2024-01-11 18:24:20 +0800

    Tidied up code, ready for recording demonstration

--- Bad Apple (console with ASCII)/badapple.py
+++ Bad Apple (console with ASCII)/badapple.py
@@ -1,23 +1,3 @@
-'''
-Version 3 of my Bad Apple program
-Bad Apple but on console in ASCII characters
-
-Written from scratch after learning the theory behind "Bad Apple Programs" from:
-- Junferno's youtube channel: https://www.youtube.com/@Junferno
-- CalvinLoke's touhou_bad_apple_v4.0.py, Github: https://github.com/CalvinLoke/bad-apple
-- LordTony's youtube video: https://www.youtube.com/watch?v=FkNkOVj7suo
-
-Raw video sourced from Bad Apple Youtube video: https://www.youtube.com/watch?v=FtutLA63Cp8
-
-Instructions to use
--------------------
-Viewport Settings:
-160 by 45 characters is the default viewport size and can comfortably compute the default badapple.mp4 in real time
-Setting the viewport to 320 by 75 characters is possible for good systems
-Anything higher may lead to lower than original fps and thus audio syncing issues
-
-Note these options are tested with the 360p, 30fps badapple.mp4 video
-'''
 import cv2
 from PIL import Image
 import os
